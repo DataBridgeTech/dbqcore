d0b13449a6b688c69b4b7c82c507d74d8f3e3668
sholokhov <sholokhov.aa@gmail.com>
2025-05-19 19:58:22 +0200

    licence headers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dbqcore
-DataBridge Quality Core library
+
+DataBridge Quality Core library is a part of [dbqctl](https://github.com/DataBridgeTech/dbqctl).
--- checks_config.go
+++ checks_config.go
@@ -1,10 +1,18 @@
+// Copyright 2025 The DBQ Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package dbqcore
-
-import (
-	"os"
-
-	"gopkg.in/yaml.v3"
-)
 
 type OnFailAction string
 
@@ -29,20 +37,4 @@ type Check struct {
 	Description string       `yaml:"description,omitempty"` // optional
 	OnFail      OnFailAction `yaml:"on_fail,omitempty"`     // optional (error, warn)
 	Query       string       `yaml:"query,omitempty"`       // optional raw query
-}
-
-func LoadChecksConfig(fileName string) (*ChecksConfig, error) {
-	file, err := os.Open(fileName)
-	defer file.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	var cfg ChecksConfig
-	decoder := yaml.NewDecoder(file)
-	if err := decoder.Decode(&cfg); err != nil {
-		return nil, err
-	}
-
-	return &cfg, nil
 }
